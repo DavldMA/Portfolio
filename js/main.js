1ff7d5a162ad4d3928bfe9dbeceb4ad1b52779c6
DavldMA <imaginaqueumghoul@gmail.com>
2024-03-15 00:04:16 +0000

    just some animations and tests

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+const cursorDot = document.querySelector(".cursor-dot");
+const cursorOutline = document.querySelector(".cursor-outline");
+var links = document.querySelectorAll('a');
+
 window.addEventListener("load", () => {
     const loader = document.querySelector(".loader");
     loader.classList.add("loader--hidden");
@@ -9,8 +13,7 @@ window.addEventListener("load", () => {
     });
 });
 
-const cursorDot = document.querySelector(".cursor-dot");
-const cursorOutline = document.querySelector("[data-cursor-outline]");
+
 
 window.addEventListener("mousemove", function (e) {
     const posX = e.clientX;
@@ -19,11 +22,25 @@ window.addEventListener("mousemove", function (e) {
     cursorDot.style.left = `${posX}px`
     cursorDot.style.top = `${posY}px`
 
-    // cursorOutline.style.left = `${posX}px`
-    // cursorOutline.style.top = `${posY}px`
-
     cursorOutline.animate({
         left: `${posX}px`,
         top: `${posY}px`
     }, { duration: 500, fill: "forwards" });
 })
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+
+    function addHoverClass() {
+        cursorOutline.classList.add('cursor-outline-hover');
+    }
+
+    function removeHoverClass() {
+        cursorOutline.classList.remove('cursor-outline-hover');
+    }
+
+    links.forEach(function(link) {
+        link.addEventListener('mouseover', addHoverClass);
+        link.addEventListener('mouseout', removeHoverClass);
+    });
+});
