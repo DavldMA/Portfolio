019fdfa4126774b405335589bf3ca2617bbb1ab6
DavldMA <imaginaqueumghoul@gmail.com>
2024-03-15 14:43:29 +0000

    just organized more the code, changed the navbar and added some cool fonts, more code to refactor later

--- js/main.js
+++ js/main.js
@@ -1,6 +1,34 @@
+// Select elements
 const cursorDot = document.querySelector(".cursor-dot");
 const cursorOutline = document.querySelector(".cursor-outline");
-var links = document.querySelectorAll('a');
+const links = document.querySelectorAll('a');
+
+
+function handleCursorMove(e) {
+    const { clientX: posX, clientY: posY } = e;
+
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
+
+    cursorOutline.animate({
+        left: `${posX}px`,
+        top: `${posY}px`
+    }, { duration: 500, fill: "forwards" });
+}
+
+function handleClick() {
+    cursorOutline.classList.add("expand");
+
+    setTimeout(() => {
+        cursorOutline.classList.remove("expand");
+    }, 500);
+}
+
+function handleHoverEffect(add) {
+    return function() {
+        cursorOutline.classList.toggle('cursor-outline-hover', add);
+    };
+}
 
 window.addEventListener("load", () => {
     const loader = document.querySelector(".loader");
@@ -13,42 +41,53 @@ window.addEventListener("load", () => {
     });
 });
 
+window.addEventListener("mousemove", handleCursorMove);
+document.addEventListener('click', handleClick);
 
-
-window.addEventListener("mousemove", function (e) {
-    const posX = e.clientX;
-    const posY = e.clientY;
-
-    cursorDot.style.left = `${posX}px`
-    cursorDot.style.top = `${posY}px`
-
-    cursorOutline.animate({
-        left: `${posX}px`,
-        top: `${posY}px`
-    }, { duration: 500, fill: "forwards" });
-})
-
-document.addEventListener('click', () => {
-    cursorOutline.classList.add("expand");
-
-    setTimeout(() => {
-        cursorOutline.classList.remove("expand");
-    }, 500)
-})
-
-document.addEventListener('DOMContentLoaded', function() {
-    
-
-    function addHoverClass() {
-        cursorOutline.classList.add('cursor-outline-hover');
-    }
-
-    function removeHoverClass() {
-        cursorOutline.classList.remove('cursor-outline-hover');
-    }
-
-    links.forEach(function(link) {
-        link.addEventListener('mouseover', addHoverClass);
-        link.addEventListener('mouseout', removeHoverClass);
+document.addEventListener('DOMContentLoaded', () => {
+    links.forEach(link => {
+        link.addEventListener('mouseover', handleHoverEffect(true));
+        link.addEventListener('mouseout', handleHoverEffect(false));
     });
 });
+
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('.menu a').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+      console.log('click');
+        
+        $('.menu a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('.menu a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.menu a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
