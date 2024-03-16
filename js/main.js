c6e20eb2ffdad65f6f0b8b36e9281743823f8cf0
DavldMA <imaginaqueumghoul@gmail.com>
2024-03-16 02:24:47 +0000

    a animation css and js for fade in

--- js/main.js
+++ js/main.js
@@ -95,3 +95,42 @@ document.addEventListener('DOMContentLoaded', function() {
 
     updateActiveClass();
 });
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+// var hasTouchScreen = false;
+
+// if ("maxTouchPoints" in navigator) {
+//     hasTouchScreen = navigator.maxTouchPoints > 0;
+// } else if ("msMaxTouchPoints" in navigator) {
+//     hasTouchScreen = navigator.msMaxTouchPoints > 0;
+// } else {
+//     var mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+//     if (mQ && mQ.media === "(pointer:coarse)") {
+//         hasTouchScreen = !!mQ.matches;
+//     } else if ('orientation' in window) {
+//         hasTouchScreen = true; // deprecated, but good fallback
+//     } else {
+//         // Only as a last resort, fall back to user agent sniffing
+//         var UA = navigator.userAgent;
+//         hasTouchScreen = (
+//             /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+//             /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
+//         );
+//     }
+// }
+
+// if (hasTouchScreen)
+//     // Do something here. 
+// }
